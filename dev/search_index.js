b250c7bc082081717cc50e1b3aa71a9a7d20e405
Documenter.jl <documenter@juliadocs.github.io>
2022-11-18 02:13:45 +0000

    build based on 98d4d8c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JADE191","category":"page"},{"location":"#JADE191","page":"Home","title":"JADE191","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JADE191.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JADE191]","category":"page"}]
+}
