161e4c801adbc12b42f65d6c67bfdbecebf36f62
kevkevs <22803504+kevkevs@users.noreply.github.com>
2022-11-17 21:22:41 -0500

    add style, manifest, and tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
+using Aqua
+using Documenter
 using JADE191
+using JuliaFormatter
 using Test
 
+DocMeta.setdocmeta!(
+    MyJuliaPackage,
+    :DocTestSetup,
+    :(using MyJuliaPackage);
+    recursive=true
+)
+
 @testset "JADE191.jl" begin
-    # Write your tests here.
+    @testset verbose = true "Code quality (Aqua.jl)" begin
+        Aqua.test_all(MyJuliaPackage; ambiguities=false)
+    end
+
+    @testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
+        @test format(MyJuliaPackage; verbose=true, overwrite=false)
+    end
+
+    @testset verbose = true "Doctests (Documenter.jl)" begin
+        doctest(MyJuliaPackage)
+    end
+
+    @testset verbose = true "My own tests" begin
+	@test 1 + 1 == 2
+    end
 end
