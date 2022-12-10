59a9159764712e4078257920d1173d5e8c12fc76
kevkevs <22803504+kevkevs@users.noreply.github.com>
2022-12-09 22:48:03 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,9 +26,102 @@ DocMeta.setdocmeta!(JADE191, :DocTestSetup, :(using JADE191); recursive=true)
 
     @testset verbose = true "Check adjacency matrix" begin
         input_path = joinpath(@__DIR__, "data", "example_input.txt")
-        output_path = joinpath(@__DIR__, "data", "example_output.txt")
         city = read_city(input_path)
         expected = Dict(2 => (3, 45, 200), 3 => (2, 45, 200), 1 => (2, 30, 250))
         @test buildAdjacencyMatrix(city) == expected
+    end
+
+    @testset verbose "Check boundary false 1" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        result = is_out_of_bounds(
+            city.starting_junction, 3, bounding_longitude_data, bounding_latitude_data, city
+        )
+        @test result == false
+    end
+
+    @testset verbose "Check boundary false 2" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        result = is_out_of_bounds(
+            city.starting_junction, 668, bounding_longitude_data, bounding_latitude_data, city
+        )
+        @test result == false
+    end
+
+    @testset verbose "Check boundary true" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        result = is_out_of_bounds(
+            city.starting_junction, 2, bounding_longitude_data, bounding_latitude_data, city
+        )
+        @test result == true
+    end
+
+    @testset verbose "Check getNextVertex is None" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        car = 1
+        vertex = 2
+        duration = 500
+        adjacencyMatrix = buildAdjacencyMatrix(city)
+        visited = Set{Tuple{Int64,Int64}}()
+        push!(visited, (2,9573))
+
+        result = getNextVertex()
+        @test result == (nothing, nothing)
+    end
+
+    @testset verbose "Check getNextVertex is None 2" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        car = 1
+        vertex = 1
+        duration = 5000
+        adjacencyMatrix = buildAdjacencyMatrix(city)
+        visited = Set{Tuple{Int64,Int64}}()
+        
+        result = getNextVertex()
+        @test result == (nothing, nothing)
+    end
+
+    @testset verbose "Check getNextVertex passes" begin
+        bounding_longitude_data, bounding_latitude_data = read_from_file(
+            "src/dividing-line-coordinates.txt"
+        )
+
+        city = read_city()
+        car = 1
+        vertex = 4
+        duration = 5000
+        adjacencyMatrix = buildAdjacencyMatrix(city)
+        visited = Set{Tuple{Int64,Int64}}()
+        
+        result = getNextVertex()
+        @test result == (2122, 9)
+    end
+
+    @testset verbose "Test compute_total_distance_traveled" begin
+        input_path = joinpath(@__DIR__, "data", "example_input.txt")
+        city = read_city(input_path)
+        adjacencyMatrix = buildAdjacencyMatrix(city)
+        
+        result = compute_total_distance_traveled(adjacencyMatrix, [1, 2, 3])
+        @test result == 450
     end
 end
